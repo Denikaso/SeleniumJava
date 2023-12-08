01014f3b787493a742904bb6209c1e39a1f060d2
Denikaso <daniil.tata@mail.ru>
2023-12-08 21:10:38 +0300

    добавлена возможность выбора маршрута на сайте https://m.cdsvyatka.com/ и вывод списка остановок маршрута

---
+++ src/main/java/org/example/BusStopsTest.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BusStopsTest {
+    private WebDriver driver;
+
+    public BusStopsTest(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void execute() {
+        // Шаг 1: Открытие сайта
+        driver.get("https://m.cdsvyatka.com/");
+
+        // Находим выпадающий список маршрутов
+        Select routeDropdown = new Select(driver.findElement(By.id("marshlist")));
+
+        // Выбираем нужный маршрут (например, 23 автобус)
+        routeDropdown.selectByValue("1023");
+
+        // Находим кнопку "Найти" внутри формы
+        WebElement searchButton = driver.findElement(By.xpath("//form[@id='marshSearch']//input[@value='Найти']"));
+
+        // Нажимаем кнопку для выполнения поиска
+        searchButton.click();
+
+        // Ожидание загрузки новой страницы (например, по заголовку)
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.titleContains("Остановки на маршруте"));
+
+        // Шаг 2: Получение списка остановок
+        List<WebElement> stopElements = driver.findElements(By.xpath("//a[contains(@href, 'prediction.php?busstop=')]"));
+
+        // Используем множество для хранения уникальных значений
+        Set<String> uniqueStops = new HashSet<>();
+
+        for (WebElement stopElement : stopElements) {
+            uniqueStops.add(stopElement.getText());
+        }
+
+        // Выводим уникальные остановки в консоль
+        System.out.println("Список остановок:");
+        for (String uniqueStop : uniqueStops) {
+            System.out.println(uniqueStop);
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,24 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.setProperty("webdriver.edge.driver", "C:\\Users\\danii\\Downloads\\msedgedriver.exe");
+        // Инициализация WebDriver
+        WebDriver driver = new EdgeDriver();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        try {
+            // Создание экземпляра BusStopsTest
+            BusStopsTest busStopsTest = new BusStopsTest(driver);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            // Вызов метода execute
+            busStopsTest.execute();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        // Вы можете добавить здесь код для дополнительных действий или просто пропустить этот блок
     }
 }
